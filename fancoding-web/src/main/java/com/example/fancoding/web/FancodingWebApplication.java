f2bebbed5aa159884ca41a1b63c22b550acec377
nanan <wangnannan@bonc.com.cn>
2023-11-08 17:58:42 +0800

    搭建一个聚合项目，并且进行多配置文件管理

---
+++ fancoding-web/src/main/java/com/example/fancoding/web/FancodingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.fancoding.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancodingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FancodingWebApplication.class, args);
+    }
+
+}
---
+++ fancoding-web/src/test/java/com/example/fancoding/web/FancodingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fancoding.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancodingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
